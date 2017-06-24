beaf28c383d5f3124ff05961945bef849a3b396b
Aaron Liao <aliao4@uci.edu>
2017-06-24 16:17:53 -0700

    a

--- webhook/index.php
+++ webhook/index.php
@@ -1,11 +1,4 @@
-<?php 
+<?php
 
-if(isset($_REQUEST['hub_challenge'])){
-	$challenge = $_REQUEST['hub_challenge'];
-	$token = $_REQUEST['hub_verify_token'];
-}
-
-if($token == "multicaster_verif_token"){
-	echo $challenge;
-}
+print_r($_GET["hub_challenge"]);
 
